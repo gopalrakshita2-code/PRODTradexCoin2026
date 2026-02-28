a0fa966c89d0fc938f96f1e09f479f9e010b3e91
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-02-28 22:36:55 +0700

    feat :load add images

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const loanController = require('../controllers/loanController');
+
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // Private routes
 router.post('/dashboard', userController.dashboard);
 router.put('/update-user-data', userController.updateUserData);
 router.post('/trade/history', userController.tradeHistory);
+router.post('/add/loan', upload.single('file'), loanController.addLoan);
 module.exports = router;
