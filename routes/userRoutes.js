549f8ec5f7ad928d4e8ecf8faffdaf0a9ad823c5
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-03-27 15:53:44 -0400

    feat:withdraw api create and list

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const loanController = require('../controllers/loanController');
 const multer = require('multer');
 const { apiRateLimiter } = require('../middleware/apiRateLimiter');
 const upload = multer({ storage: multer.memoryStorage() });
+const withdrawController = require('../controllers/withdrawController');
 const { authenticate } = require('../middleware/auth');
 
 // Private routes
@@ -13,4 +14,5 @@ router.post('/dashboard',authenticate, userController.dashboard);
 router.put('/update-user-data',authenticate ,userController.updateUserData);
 router.post('/trade/history',authenticate, userController.tradeHistory);
 router.post('/add/loan',authenticate, upload.single('file'), apiRateLimiter(), loanController.addLoan);
+router.post('/withdraw/funds',authenticate, apiRateLimiter(), withdrawController.WithDrawFunds);
 module.exports = router;
