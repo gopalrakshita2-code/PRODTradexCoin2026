a408caa28d3a6bb8ac266a154d90b8860bcd0788
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-04-10 14:43:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const corsOptions = {
 };
 
 // Middleware
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(require('cors')());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
